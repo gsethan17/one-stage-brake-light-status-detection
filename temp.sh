0c6154d0c5f3dcda1c5441b07ae7c06d6b86003c
Geesung Oh <gsethan17@gmail.com>
2023-07-03 20:47:21 +0900

    typeerror

--- temp.sh
+++ temp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-!wget -O test_video.mp4 https://www.dropbox.com/s/mesjnk350f0yio8/day_city.mp4?dl=0
-!wget -O yolov8x_ct.onnx https://www.dropbox.com/s/9mka105rpoax8wy/yolov8x_ct.onnx?dl=0
-!python3 onnx_infer.py --source test_video.mp4  --model_size x --is_show no --is_to_end no
+wget -O test_video.mp4 https://www.dropbox.com/s/mesjnk350f0yio8/day_city.mp4?dl=0
+wget -O yolov8x_ct.onnx https://www.dropbox.com/s/9mka105rpoax8wy/yolov8x_ct.onnx?dl=0
+python3 onnx_infer.py --source test_video.mp4  --model_size x --is_show no --is_to_end no
